cf7419742ed002d604a27130845ab97c4b67b7e4
Charity Jackson <charitylennjackson@yahoo.com>
2018-05-04 22:11:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
  
  function deleteFromObjectByKey(object, key){
   var object3 = Object.assign({}, object);
-   delete object.key;
+   delete object3.key;
    return object3;
  }
