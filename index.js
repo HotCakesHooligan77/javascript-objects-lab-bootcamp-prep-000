332e8b6c5ef52bda89cdd679e1a2a01147d13320
Charity Jackson <charitylennjackson@yahoo.com>
2018-05-04 20:10:53 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+var recipes = new Object;
+
+ function updateObjectWithKeyAndValue(object, key, value){
+  var obj1 = object[key] = value;
+  return obj1;
+   
+ }
+
+ function  destructivelyUpdateObjectWithAndValue(object, key, value){
+    
+  }
