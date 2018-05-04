d2291abdea5d4ba21e0f188508cbd27aa196a32f
Charity Jackson <charitylennjackson@yahoo.com>
2018-05-04 21:56:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = new Object;
 
  function updateObjectWithKeyAndValue(object, key, value){
- var object2 = Object.assign({},object,{key:value});
+ var object2 = Object.assign({},object,{[key]:value});
   return object2;
   
   
